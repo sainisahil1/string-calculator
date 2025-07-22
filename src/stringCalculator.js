4ca80337e1cec318d2630e1c3cc4b97a63a6f14c
Sahil <sahils123saini@gmail.com>
2025-07-22 12:00:16 +0530

    refactor to clean code

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -4,23 +4,34 @@ function add(numbers){
     let delimiter = /,|\n/;
     let numStr = numbers;
 
-    if(numbers.startsWith('//')){
+    ({ delimiter, numStr } = checkForCustomDelimiter(numbers, delimiter, numStr));
+    const parsedNums = parseStringToInt(numStr, delimiter);
+    checkForNegatives(parsedNums);
+    const sum = parsedNums.reduce((acc, curr) => acc + curr, 0);
+
+    return sum;
+}
+
+function checkForCustomDelimiter(numbers, delimiter, numStr) {
+    if (numbers.startsWith('//')) {
         const parts = numbers.split('\n');
         delimiter = new RegExp(parts[0].slice(2));
         numStr = parts[1];
     }
-    
+    return { delimiter, numStr };
+}
+
+function parseStringToInt(numStr, delimiter) {
     const splitNums = numStr.split(delimiter);
     const parsedNums = splitNums.map(n => parseInt(n));
+    return parsedNums;
+}
 
+function checkForNegatives(parsedNums) {
     const negativeNums = parsedNums.filter(n => n < 0);
-    if(negativeNums.length > 0){
+    if (negativeNums.length > 0) {
         throw new Error(`Negatives not allowed: ${negativeNums.join(', ')}`);
     }
-
-    const sum = parsedNums.reduce((acc, curr) => acc + curr, 0);
-
-    return sum;
 }
 
 module.exports = { add };
