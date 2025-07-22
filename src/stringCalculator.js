4b0d39fdf7df25e6e20d19cdfca2bbe5ea946029
Sahil <sahils123saini@gmail.com>
2025-07-22 10:43:06 +0530

    test case 2

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,5 +1,6 @@
 function add(numbers){
     if(numbers === '') return 0;
+    if(!numbers.includes(',') && !numbers.includes('\n')) return parseInt(numbers);
     return 0;
 }
 
--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -4,4 +4,8 @@ describe('String Calculator', () => {
     test('should return 0 for an empty string', () => { 
         expect(add('')).toBe(0)
      })
+
+     test('should return the number only if one number is provided', () => { 
+        expect(add('5')).toBe(5)
+      })
 })
